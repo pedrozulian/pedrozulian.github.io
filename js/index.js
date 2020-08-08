7ff919efe150ad6cc6714b878a0460b878b8a1ba
Pedro Zulian <pedrotzf@gmail.com>
2020-08-08 10:13:41 -0300

    create ul; with li's of commits

--- js/index.js
+++ js/index.js
@@ -16,6 +16,38 @@ ajax.onreadystatechange = function() {
         
         this.listCommits = data.slice(0, 10);
         this.listCommits = getCommitsFromGithub(this.listCommits);
+
+        this.listCommits.forEach((commit) => {
+            const divTarget = document.getElementById("commits"); // <li> with id="commits"
+
+            const divCommit = document.createElement("li");
+            divCommit.classList.add("commit", "commit-timeline");
+
+            // element of message
+            const message = document.createElement("span");
+            message.classList.add("message", "mt-3");
+            message.innerHTML = commit.message;
+
+            // element of committed (with link of commit on github)
+            const committed = document.createElement("span");
+            const committed_link = document.createElement("a");
+            committed.classList.add("committed");
+            committed_link.innerHTML = "committed";
+            committed_link.setAttribute("href", commit.url);
+            committed_link.classList.add("committed-text");
+            committed.appendChild(committed_link);
+
+            // element with name of repository
+            const repository = document.createElement("span");
+            repository.innerHTML = "on repository " + commit.repository;
+
+
+            divCommit.appendChild(message);
+            divCommit.appendChild(committed);
+            divCommit.appendChild(repository);
+            divTarget.appendChild(divCommit);
+        });
+
     }
 }
 
