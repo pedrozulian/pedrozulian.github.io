0e411c54a1ac5a18493886ec636142dbfd3e64a1
Pedro <pedrotzf@gmail.com>
2021-03-02 20:53:53 -0300

    feat(app.module.ts): principais modulos usados ou que ainda vão ser
    utilizados

--- my-website/src/app/app.module.ts
+++ my-website/src/app/app.module.ts
@@ -4,13 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MeusCommitsComponent } from './components/meus-commits/meus-commits.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeusCommitsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
