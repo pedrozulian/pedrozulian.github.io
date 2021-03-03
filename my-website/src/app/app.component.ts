e000fe81bc7535419367432303043cdcd77027ea
Pedro <pedrotzf@gmail.com>
2021-03-02 21:03:23 -0300

    feat(app.component.ts): delay do tooltip ao desaparecer

--- my-website/src/app/app.component.ts
+++ my-website/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {}
+export class AppComponent {
+  hideDelay = new FormControl(150);
+}
