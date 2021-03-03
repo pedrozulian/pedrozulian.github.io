7d7fe0b9ac859432e94c3b914c64193819093327
Pedro <pedrotzf@gmail.com>
2021-03-03 18:27:33 -0300

    feat(app.module.ts): importando shared module e melhorando
    organização das importações

--- my-website/src/app/app.module.ts
+++ my-website/src/app/app.module.ts
@@ -1,19 +1,22 @@
+// Default imports from Angular
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatTooltipModule  } from '@angular/material/tooltip';
-import { ActivitiesModule } from './modules/activities/activities.module';
 
+// Modules inside project: Modules, Shared...
+import { AppRoutingModule } from './app-routing.module';
+import { ActivitiesModule } from './modules/activities/activities.module';
+import { SharedModule } from './core/shared/components/last-commits/shared.module';
 
 @NgModule({
   declarations: [
@@ -31,7 +34,8 @@ import { ActivitiesModule } from './modules/activities/activities.module';
     MatListModule,
     MatTooltipModule,
     HttpClientModule,
-    ActivitiesModule
+    ActivitiesModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
