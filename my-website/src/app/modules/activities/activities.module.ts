6ac7af1079d362d83c91c45545a1fea9f1bd7d30
Pedro <pedrotzf@gmail.com>
2021-03-04 18:53:48 -0300

    feat(activities.module.ts): alguns imports e organização do arquivo

--- my-website/src/app/modules/activities/activities.module.ts
+++ my-website/src/app/modules/activities/activities.module.ts
@@ -1,15 +1,26 @@
+// Default from Angular
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// Modules from Angular Material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+
+// Modules and Components from App
+import { SharedModule } from 'src/app/shared/components/last-commits/shared.module';
 import { ActivitiesComponent } from './activities.component';
-
-
 
 @NgModule({
   declarations: [
     ActivitiesComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatDividerModule,
+    SharedModule
   ],
   exports: [
     ActivitiesComponent
