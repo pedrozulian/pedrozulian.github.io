448b14991323b5bfeeef1365bd7ce8bdf72e8dbc
Pedro <pedrotzf@gmail.com>
2021-03-05 16:29:12 -0300

    feat(github.service.ts): rota que busca infos de repositórios

--- my-website/src/app/core/services/github.service.ts
+++ my-website/src/app/core/services/github.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class GithubService {
 
   public getEventsGithub() {
     return this.http.get('https://api.github.com/users/pedrozulian/events');
+  }
+
+  public getRepositoriesGithub() {
+    return this.http.get('https://api.github.com/users/pedrozulian/repos');
   }
 }
