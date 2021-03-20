76286ff7b94773c2a6f1380fe2b93d77f1397ecc
Pedro <pedrotzf@gmail.com>
2021-03-20 18:36:09 -0300

    organizando tbm o layout.module

--- website/src/app/modules/layout/layout.module.ts
+++ website/src/app/modules/layout/layout.module.ts
@@ -1,13 +1,22 @@
+// Angular default
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// Modules
+
+// Components
+import { LayoutComponent } from './layout.component';
 import { HeaderComponent } from './components/header/header.component';
 
-
-
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [
+    LayoutComponent,
+    HeaderComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    LayoutComponent
   ]
 })
 export class LayoutModule { }
