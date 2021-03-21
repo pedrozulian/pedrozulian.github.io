aeb8a88b80ef2a81d3c2c9f57d21a16efe56161a
Pedro <pedrotzf@gmail.com>
2021-03-21 20:18:08 -0300

    feat(panel.components.ts): create a items to display on panel

--- website/src/app/modules/home/components/panel/panel.component.ts
+++ website/src/app/modules/home/components/panel/panel.component.ts
@@ -9,6 +9,18 @@ export class PanelComponent implements OnInit {
 
   constructor() { }
 
+  panelItems: Object[] = [
+    { image: 'undraw_building_websites.svg', imageAlt: 'Building Softwares',
+     title: 'Desenvolvimentos que participei', 
+     description: 'Softwares em que estive presente no desenvolvimento, seja profissionalmente ou relacionados a estudos.' },
+
+    { image: 'undraw_developer_activity.svg', imageAlt: 'Developer Activity',
+     title: 'Minhas últimas atividades', description: 'Estudos e projetos pessoais meus estão armazenados em meu repositório no Github. Fique à vontade para explorar, clonar, contribuir!' },
+
+    { image: 'undraw_sharing_articles.svg', imageAlt: 'Sharing Articles',
+     title: 'Contribuições para comunidade', description: 'Aqui estão minhas principais publicações e vídeos em que compartilho o conhecimento, afinal, ninguém cresce sozinho.' }
+  ];
+
   ngOnInit(): void {
   }
 
