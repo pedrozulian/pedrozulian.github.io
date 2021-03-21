be2bb96cf7bc470b5331e3b149b486401afabeac
Pedro <pedrotzf@gmail.com>
2021-03-21 19:12:14 -0300

    feat(panel-item.component.ts): add some Input var and a function to
    join url for image on html

--- website/src/app/modules/home/components/panel-item/panel-item.component.ts
+++ website/src/app/modules/home/components/panel-item/panel-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-panel-item',
@@ -9,7 +9,16 @@ export class PanelItemComponent implements OnInit {
 
   constructor() { }
 
+  @Input() image: string;
+  @Input() imageAlt: string;
+  @Input() title: string;
+  @Input() description: string;
+
   ngOnInit(): void {
+  }
+
+  getSrcImage() {
+    return '../../../../../assets/undraw/' + this.image;
   }
 
 }
