c33eb1069bfb60ab245927b41ce59cceb5a2eb0b
Pedro <pedrotzf@gmail.com>
2021-03-22 16:49:08 -0300

    feat(panel.component.ts): insert to panelItems a path in objects

--- website/src/app/modules/home/components/panel/panel.component.ts
+++ website/src/app/modules/home/components/panel/panel.component.ts
@@ -12,14 +12,17 @@ export class PanelComponent implements OnInit {
   panelItems: Object[] = [
     { image: 'undraw_building_websites.svg', imageAlt: 'Building Softwares',
      title: 'Projetos e Softwares', 
-     description: 'Conheça os principais sistemas que desenvolvi ou participei do desenvolvimento em ambiente profissional e de estudos.' },
+     description: 'Conheça os principais sistemas que desenvolvi ou participei do desenvolvimento em ambiente profissional e de estudos.',
+     path: '#' },
 
     { image: 'undraw_developer_activity.svg', imageAlt: 'Developer Activity',
      title: 'Atividades', 
-     description: 'Meus estudos e projetos pessoais estão armazenados em meu repositório no Github. Fique à vontade para explorar, clonar e contribuir!' },
+     description: 'Meus estudos e projetos pessoais estão armazenados em meu repositório no Github. Fique à vontade para explorar, clonar e contribuir!',
+     path: '/atividades' },
 
     { image: 'undraw_sharing_articles.svg', imageAlt: 'Sharing Articles',
-     title: 'Contribuições', description: 'Aqui estão minhas principais publicações e vídeos em que compartilho o conhecimento, afinal, ninguém cresce sozinho.' }
+     title: 'Contribuições', description: 'Aqui estão minhas principais publicações e vídeos em que compartilho o conhecimento, afinal, ninguém cresce sozinho.',
+     path: '#' }
   ];
 
   ngOnInit(): void {
