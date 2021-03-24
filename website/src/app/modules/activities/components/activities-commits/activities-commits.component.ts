5677d89a8e5810ef5700c30c430d2763061fc90a
Pedro <pedrotzf@gmail.com>
2021-03-24 20:00:54 -0300

    feat(activities-commits.component.ts): commits array; and method to subscribe
    response of request on service

--- website/src/app/modules/activities/components/activities-commits/activities-commits.component.ts
+++ website/src/app/modules/activities/components/activities-commits/activities-commits.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from 'src/app/core/services/github.service';
+import { CommitGithub } from 'src/app/shared/models/github/commit.model';
 
 @Component({
   selector: 'app-activities-commits',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivitiesCommitsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private githubService: GithubService
+  ) { }
+
+  commits: CommitGithub[][];
 
   ngOnInit(): void {
+    this.getEvents();
+  }
+
+  getEvents() {
+    this.githubService.getCommits()
+      .subscribe(data => this.commits = data);
   }
 
 }
