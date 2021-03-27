018644deb3bf016d5aa0106f9fbd6274b5ddcca5
Pedro <pedro.zulian@nkodontologia.com.br>
2021-03-27 16:43:48 -0300

    feat(activities-repositories.component.ts): subscribe service github and copy clipboard

--- website/src/app/modules/activities/components/activities-repositories/activities-repositories.component.ts
+++ website/src/app/modules/activities/components/activities-repositories/activities-repositories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from 'src/app/core/services/github.service';
+import { RepositoryGithub } from 'src/app/shared/models/github/repository.model';
 
 @Component({
   selector: 'app-activities-repositories',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivitiesRepositoriesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private githubService: GithubService
+  ) { }
+
+  repositories: RepositoryGithub[];
 
   ngOnInit(): void {
+    this.getRepositories();
+  }
+
+  getRepositories() {
+    this.githubService.getRepositories()
+      .subscribe(data => {
+        this.repositories = data
+      });
+  }
+
+  copyTextClone(txt: string) {
+    let selTxt = document.createElement('textarea');
+    selTxt.style.position = 'fixed';
+    selTxt.style.left = '0';
+    selTxt.style.top = '0';
+    selTxt.style.opacity = '0';
+    selTxt.value = 'git clone ' + txt;
+    document.body.appendChild(selTxt);
+    selTxt.focus();
+    selTxt.select();
+    document.execCommand('copy');
+    document.body.removeChild(selTxt);
   }
 
 }
